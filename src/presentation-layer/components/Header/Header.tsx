306ad2d7d62fb19bc4cc2a7bdf3ab6e4472f3cb7
Harlencito <harlengiraldo@gmail.com>
2021-05-10 23:14:16 -0500

    Added link to the md icon

--- src/presentation-layer/components/Header/Header.tsx
+++ src/presentation-layer/components/Header/Header.tsx
@@ -2,14 +2,22 @@ import React, { FunctionComponent, useState } from "react";
 import { SearchBox } from "presentation-layer/components";
 import mlLogo from "presentation-layer/assets/mlLogo.png";
 import useQueryPath from "application/hooks/useQueryPath";
+import { Link } from "react-router-dom";
+
 
 const Header: FunctionComponent = () => {
   const [queryString] = useQueryPath();
   const [searchValue, setSearchValue] = useState(queryString?.search ?? "");
   return (
     <nav className="navbar">
-      <img src={mlLogo} alt="mercado libre logo" />
-      <SearchBox onChange={setSearchValue} value={searchValue} placeholder="Nunca dejes de sonar" />
+      <Link to="/">
+        <img src={mlLogo} alt="mercado libre logo" />
+      </Link>
+      <SearchBox
+        onChange={setSearchValue}
+        value={searchValue}
+        placeholder="Nunca dejes de sonar"
+      />
     </nav>
   );
 };
